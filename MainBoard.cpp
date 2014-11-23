34c5806e38ecad64f862cda297c037b724d2847b
ThatGuyWithTheFace <thatguywiththeface1479@gmail.com>
2014-11-23 17:52:03 -0500

    added gyro, encoder

--- MainBoard.cpp
+++ MainBoard.cpp
@@ -10,66 +10,87 @@ class SpreadBoard : public IterativeRobot{
 	
 	static const int GYRO_ANALOG = 1;
 	
+	static const int ENCODER_A = 2;
+	static const int ENCODER_B = 3;
+	
 	Gyro * gyro;
 	Victor * motor_1;
 	Victor * motor_2;
 	Victor * motor_3;
 	Gamepad * pilot;
+	Encoder * encoder;
 	
 	DriverStationLCD * lcd;
 	
-void SpreadBoard::RobotInit() {
 	
-	motor_1 = new Victor(MOTOR_ONE);
-	motor_2 = new Victor(MOTOR_TWO);
-	motor_3 = new Victor(MOTOR_THREE);
+	int NormalizedAngle(float angle){
+		int i_angle = (int) angle;
+		if (i_angle >= 0) {
+			return i_angle % 360;
+		} else {
+			return 360 + i_angle % 360;
+		}
+	}
 	
-	pilot = new Gamepad(1);
+	void RobotInit() {
+		
+		motor_1 = new Victor(MOTOR_ONE);
+		motor_2 = new Victor(MOTOR_TWO);
+		motor_3 = new Victor(MOTOR_THREE);
+		
+		pilot = new Gamepad(1);
+		
+		encoder = new Encoder(ENCODER_A, ENCODER_B);
+		encoder->Start();
+				
+		gyro = new Gyro(GYRO_ANALOG);
+		gyro->SetSensitivity(0.007f); //=7 mV/degree/sec
+		
+		lcd = DriverStationLCD::GetInstance();
+	}
 	
-	gyro = new Gyro(GYRO_ANALOG);
-	gyro->SetSensitivity(0.007f); //=7 mV/degree/sec
 	
-	lcd = DriverStationLCD::GetInstance();
-}
-
-
-void SpreadBoard::DisabledInit() {
-}
-
-
-void SpreadBoard::DisabledPeriodic() {
-}
-
-
-void SpreadBoard::AutonomousInit() {
-}
-
-
-void SpreadBoard::AutonomousPeriodic() {
-}
-
-
-void SpreadBoard::TeleopInit() {
-}
-
-
-void SpreadBoard::TeleopPeriodic() {
-	motor_1->Set(pilot->GetLeftY());
-	motor_2->Set(pilot->GetLeftX());
-	motor_3->Set(pilot->GetRightY());
+	void DisabledInit() {
+	}
 	
-	float angle = gyro->GetAngle();
-	lcd->PrintfLine(DriverStationLCD::kUser_Line1, "angle: %f", angle);
 	
-	lcd->UpdateLCD();
-}
+	void DisabledPeriodic() {
+	}
 	
-void SpreadBoard::TestInit() {
-}
+	
+	void AutonomousInit() {
+	}
+	
+	
+	void AutonomousPeriodic() {
+	}
+	
+	
+	void TeleopInit() {
+	}
+	
+	
+	void TeleopPeriodic() {
+		motor_1->Set(pilot->GetLeftY());
+		motor_2->Set(pilot->GetLeftX());
+		motor_3->Set(pilot->GetRightY());
+		
+		float angle = gyro->GetAngle();
+		
+		lcd->PrintfLine(DriverStationLCD::kUser_Line1, "gyro: %f", angle);
+		lcd->PrintfLine(DriverStationLCD::kUser_Line2, "angle: %d", NormalizedAngle(angle));
+		lcd->PrintfLine(DriverStationLCD::kUser_Line3, "enc: %d", encoder->Get());
 
-
-void SpreadBoard::TestPeriodic() {
-}
+		
+		lcd->UpdateLCD();
+	}
+		
+	void TestInit() {
+	}
+	
+	
+	void TestPeriodic() {
+	}
 };
 START_ROBOT_CLASS(SpreadBoard);
 
