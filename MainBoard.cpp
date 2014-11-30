d912b3bfd8dd7552a59f97fffeed42dca66fe32f
ThatGuyWithTheFace <thatguywiththeface1479@gmail.com>
2014-11-30 16:59:06 -0500

    added accelerometer

--- MainBoard.cpp
+++ MainBoard.cpp
@@ -19,7 +19,8 @@ class SpreadBoard : public IterativeRobot{
 	Victor * motor_3;
 	Gamepad * pilot;
 	Encoder * encoder;
-	
+	ADXL345_I2C * accelerometer;
+	PIDController * pid;
 	DriverStationLCD * lcd;
 	
 	
@@ -41,10 +42,16 @@ class SpreadBoard : public IterativeRobot{
 		pilot = new Gamepad(1);
 		
 		encoder = new Encoder(ENCODER_A, ENCODER_B);
+		encoder->SetDistancePerPulse(1.0 / 64.0); //gives number of rotations; 1 rotation = 64 pulses
+		encoder->SetPIDSourceParameter(Encoder::kRate);
 		encoder->Start();
 				
 		gyro = new Gyro(GYRO_ANALOG);
 		gyro->SetSensitivity(0.007f); //=7 mV/degree/sec
+		
+		accelerometer = new ADXL345_I2C(1); //1 is the digital module??
+		
+		pid = new PIDController(0.1, 0.0, 0.0, encoder, motor_1);
 		
 		lcd = DriverStationLCD::GetInstance();
 	}
@@ -55,6 +62,7 @@ class SpreadBoard : public IterativeRobot{
 	
 	
 	void DisabledPeriodic() {
+		lcd->Clear();
 	}
 	
 	
@@ -69,18 +77,26 @@ class SpreadBoard : public IterativeRobot{
 	void TeleopInit() {
 	}
 	
+	double clamp(double speed, double maxMagnitude){
+		if(fabs(speed) <= maxMagnitude)
+			return 0;
+		return speed;
+	}
 	
 	void TeleopPeriodic() {
-		motor_1->Set(pilot->GetLeftY());
-		motor_2->Set(pilot->GetLeftX());
-		motor_3->Set(pilot->GetRightY());
+		//motor_1->Set(clamp(pilot->GetLeftY(), 0.1));
+		motor_2->Set(clamp(pilot->GetLeftX(), 0.1));
+		motor_3->Set(clamp(pilot->GetRightY(), 0.1));
 		
 		float angle = gyro->GetAngle();
+		double rate = encoder->GetRate() * 60.0; //rpm
 		
 		lcd->PrintfLine(DriverStationLCD::kUser_Line1, "gyro: %f", angle);
 		lcd->PrintfLine(DriverStationLCD::kUser_Line2, "angle: %d", NormalizedAngle(angle));
 		lcd->PrintfLine(DriverStationLCD::kUser_Line3, "enc: %d", encoder->Get());
-
+		lcd->PrintfLine(DriverStationLCD::kUser_Line4, "rpm: %d", (int) (rate + 0.5));
+		lcd->PrintfLine(DriverStationLCD::kUser_Line5, "accel: %f", accelerometer->GetAccelerations().XAxis);
+		
 		
 		lcd->UpdateLCD();
 	}
