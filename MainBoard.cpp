10a5dcee3aff6da677331545edabb769a9301edb
ThatGuyWithTheFace <thatguywiththeface1479@gmail.com>
2014-11-30 17:59:04 -0500

    messed with pid

--- MainBoard.cpp
+++ MainBoard.cpp
@@ -42,6 +42,7 @@ class SpreadBoard : public IterativeRobot{
 		pilot = new Gamepad(1);
 		
 		encoder = new Encoder(ENCODER_A, ENCODER_B);
+		encoder->SetReverseDirection(true);
 		encoder->SetDistancePerPulse(1.0 / 64.0); //gives number of rotations; 1 rotation = 64 pulses
 		encoder->SetPIDSourceParameter(Encoder::kRate);
 		encoder->Start();
@@ -51,7 +52,10 @@ class SpreadBoard : public IterativeRobot{
 		
 		accelerometer = new ADXL345_I2C(1); //1 is the digital module??
 		
-		pid = new PIDController(0.1, 0.0, 0.0, encoder, motor_1);
+		pid = new PIDController(2.3, 0.0, 0.5, encoder, motor_1);
+		pid->SetInputRange(0.0, 1.4); //approximate range of encoder values
+		pid->SetOutputRange(0.0, 1.0); //valid victor inputs (we don't want to go backwards)
+		pid->SetSetpoint(1.0f); //1 rps
 		
 		lcd = DriverStationLCD::GetInstance();
 	}
@@ -63,6 +67,7 @@ class SpreadBoard : public IterativeRobot{
 	
 	void DisabledPeriodic() {
 		lcd->Clear();
+		pid->Disable();
 	}
 	
 	
@@ -88,14 +93,28 @@ class SpreadBoard : public IterativeRobot{
 		motor_2->Set(clamp(pilot->GetLeftX(), 0.1));
 		motor_3->Set(clamp(pilot->GetRightY(), 0.1));
 		
-		float angle = gyro->GetAngle();
-		double rate = encoder->GetRate() * 60.0; //rpm
+		pid->Enable();
 		
-		lcd->PrintfLine(DriverStationLCD::kUser_Line1, "gyro: %f", angle);
-		lcd->PrintfLine(DriverStationLCD::kUser_Line2, "angle: %d", NormalizedAngle(angle));
-		lcd->PrintfLine(DriverStationLCD::kUser_Line3, "enc: %d", encoder->Get());
-		lcd->PrintfLine(DriverStationLCD::kUser_Line4, "rpm: %d", (int) (rate + 0.5));
-		lcd->PrintfLine(DriverStationLCD::kUser_Line5, "accel: %f", accelerometer->GetAccelerations().XAxis);
+		float angle = gyro->GetAngle();
+		double rate = encoder->PIDGet(); //rps
+		
+		ADXL345_I2C::AllAxes accel = accelerometer->GetAccelerations();
+		//convert from G's to ft/s^2 (1G = 32ft/s^2
+		accel.XAxis *= 32.0;
+		accel.YAxis *= 32.0;
+		accel.ZAxis *= 32.0;
+		double accel_mag = sqrt(pow(accel.XAxis, 2) + pow(accel.YAxis, 2) + pow(accel.ZAxis, 2));
+		
+		//lcd->PrintfLine(DriverStationLCD::kUser_Line1, "gyro: %f", angle);
+		lcd->PrintfLine(DriverStationLCD::kUser_Line1, "angle: %d", NormalizedAngle(angle));
+		//lcd->PrintfLine(DriverStationLCD::kUser_Line3, "enc: %d", encoder->Get());
+		lcd->PrintfLine(DriverStationLCD::kUser_Line2, "rps: %f", rate);
+		lcd->PrintfLine(DriverStationLCD::kUser_Line3, "spt: %f", pid->GetSetpoint());
+		//lcd->PrintfLine(DriverStationLCD::kUser_Line3, "x: %d", (int) accel.XAxis);
+		//lcd->PrintfLine(DriverStationLCD::kUser_Line4, "y: %d", (int) accel.YAxis);
+		//lcd->PrintfLine(DriverStationLCD::kUser_Line5, "z: %d", (int) accel.ZAxis);
+		lcd->PrintfLine(DriverStationLCD::kUser_Line6, "mag: %d", (int) accel_mag);
+		
 		
 		
 		lcd->UpdateLCD();
